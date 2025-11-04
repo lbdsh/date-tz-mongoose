5758b79b0f5c12ddf0935e88c939b9134d9926ed
Antonino <77512552+antonino-t@users.noreply.github.com>
2025-11-04 18:02:11 +0100

    first commit

---
+++ src/datetz-schema.ts
@@ -0,0 +1,24 @@
+import { DateTz, IDateTz } from "@lbd-sh/date-tz";
+import mongoose, { SchemaType } from "mongoose";
+
+
+export class DateTzSchema extends SchemaType {
+  constructor(key: string, options: any) {
+    super(key, options, 'DateTzSchema');
+  }
+
+  cast(data: any): IDateTz | undefined {
+    if (data instanceof DateTz) {
+      return data;
+    }
+    if (data && typeof data === 'object' && typeof data.timestamp === 'number' && typeof data.timezone === 'string') {
+      return new DateTz(data.timestamp, data.timezone);
+    }
+    // if (typeof data === 'string') {
+    //   return DateTz.parse(data, DateTz.defaultFormat);
+    // }
+    return undefined;
+  }
+}
+
+mongoose.Schema.Types['DateTzSchema'] = DateTzSchema;
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export * from './datetz-schema';
+
